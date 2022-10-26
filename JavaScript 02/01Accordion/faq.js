f932bd948f9e308aa93bb1cb05607a2c57aaef17
sub1120 <subhamsahu1120@gmail.com>
2022-10-26 13:15:59 +0530

    completed accordian proj

--- JavaScript 02/01Accordion/faq.js
+++ JavaScript 02/01Accordion/faq.js
@@ -19,40 +19,42 @@ const faqData = [
 ];
 
 const accordianBody = document.querySelector(".accordian_body");
-const faqs = [];
 
-function showFaq() {}
+function loadFaqs() {
 
-function createFaq(faqData) {
-  const faqDiv = document.createElement("div");
-  faqDiv.setAttribute("class", "faq");
-  faqDiv.setAttribute("id", `element-id-${faqData.id}`);
-  accordianBody.appendChild(faqDiv);
+  faqData.forEach(data => {
+    const faqDiv = document.createElement("div");
+    faqDiv.setAttribute("class", "faq");
+    faqDiv.setAttribute("id", `element-id-${data.id}`);
 
-  const faqHeaderDiv = document.createElement("div");
-  faqHeaderDiv.setAttribute("class", "faq_header");
-  faqDiv.appendChild(faqHeaderDiv);
+    const faqHeaderDiv = document.createElement("div");
+    faqHeaderDiv.setAttribute("class", "faq_header");
+    faqDiv.appendChild(faqHeaderDiv);
 
-  const faqHeading = document.createElement("h3");
-  faqHeading.innerText = faqData.question;
-  faqHeaderDiv.appendChild(faqHeading);
+    const faqHeading = document.createElement("h3");
+    faqHeading.innerText = data.question;
+    faqHeaderDiv.appendChild(faqHeading);
 
-  const faqBtn = document.createElement("button");
-  faqBtn.setAttribute("class", `show_btn`);
-  faqBtn.setAttribute("id", `element-id-${faqData.id}`);
-  faqBtn.innerText = "+";
-  faqBtn.addEventListener("click", btnStatusUpdate);
-  faqHeaderDiv.appendChild(faqBtn);
+    const faqBtn = document.createElement("button");
+    faqBtn.setAttribute("class", `show_btn`);
+    faqBtn.setAttribute("id", `element-id-${data.id}`);
+    faqBtn.innerText = "+";
+    faqBtn.addEventListener("click", faqHandler);
+    faqHeaderDiv.appendChild(faqBtn);
 
-  const faqAns = document.createElement("p");
-  faqAns.setAttribute("class", "hidden");
-  faqAns.innerText = faqData.answer;
-  faqDiv.appendChild(faqAns);
+    const faqAns = document.createElement("p");
+    faqAns.setAttribute("class", "hidden");
+    faqAns.innerText = data.answer;
+    faqDiv.appendChild(faqAns);
+  
+    accordianBody.appendChild(faqDiv);
+  })
 }
 
-function btnStatusUpdate(event) {
+function faqHandler(event) {
   const elementId = event.target.id;
   const faqAns = document.querySelector(`.faq#${elementId} p`);
+
   if (faqAns.className == "") {
     event.target.innerText  = "+"
     faqAns.setAttribute("class", "hidden");
@@ -62,6 +64,4 @@ function btnStatusUpdate(event) {
   }
 }
 
-for (const faq of faqData) {
-  createFaq(faq);
-}
+loadFaqs()
