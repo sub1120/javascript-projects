e00c7ecd851dd96793fbf39477de4adc3586462e
sub1120 <subhamsahu1120@gmail.com>
2022-10-31 20:23:39 +0530

    added products api

--- JavaScript 03/cartApp/script.js
+++ JavaScript 03/cartApp/script.js
@@ -1,60 +1,100 @@
-const products = [{
-        key:1,
-        productName:"ASUS VivoBook 15 (2022) Core i3 10th Gen",
-        productCost:100,
-    },
-    {   
-        key:2,
-        productName:"Boult Audio AirBass GearPods",
-        productCost:200,
-    },
-    {   
-        key:3,
-        productName:"Samsung Watch 4, 44mmSuper amled",
-        productCost:400,
-    },
-    {   
-        key:4,
-        productName:"Canon EOS 3000D DSLR Camera 1 Camera Body",
-        productCost:900,
-    },
-    {
-        key:5,
-        productName:"Mi 18W Quick Charger for Mi,Redmi,Xiomi",
-        productCost:100,
-    },
-]
-
+let products = null;
 let cartProducts = []
-const productList = document.querySelector(".product-list");
-const cartProductList = document.querySelector(".cart-product-list");
-const totalCost = document.querySelector('.total');
+
+const cartProductList = document.querySelector("#cart-product-list");
+const productList = document.querySelector("#product-list");
 const itemsCount = document.querySelector('#total-items');
+const totalCost = document.querySelector('#total');
+
+async function loadProducts(){
+    try{
+        const productsJson = await fetch("https://fakestoreapi.com/products");
+        products = await productsJson.json();
+        renderProducts()
+    }catch(error){
+        console.log(error);
+    }
+}
+
+const renderProducts = () => {
+    products.forEach(item => {
+        const newProduct = `<div class="card mb-3" style="max-width: 100%;">
+            <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${item.image}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+                <p class="card-text">${item.price} Rs</p>
+                <p class="card-text">${item.description}</p>
+                <p class="card-text"><small class="text-muted">Rated ${item.rating.rate} by ${item.rating.count} peoples</small></p>
+                <div><button id=${item.id} class="add btn btn-primary">Add to Cart</button></div>  
+                </div>
+            </div>
+            </div>
+        </div>`
+        
+        productList.innerHTML += newProduct;
+    })
+
+    document.querySelectorAll(".add").forEach((product) => product.addEventListener("click", addToCart));
+}
+
+const renderCartProducts = () => {
+    cartProductList.innerHTML = null;
+    let total = 0;
+    let totalItems = 0;
+    
+    cartProducts.forEach( item => {
+        const newProduct = `<li class="list-group-item">
+            <div class="cart-product">
+                <div>${item.title.slice(0, 20)}, at  ${item.price} Rs</div>
+                <div><input class="cart-input" type="number" id="${item.id}" value="${item.count}"/></div>
+            </div>
+        </li>`;
+
+        cartProductList.innerHTML += newProduct;
+
+        total += item.count*item.price;
+        totalItems += item.count;
+    })
+
+    totalCost.innerText = `${Math.ceil(total)} Rs`;
+    itemsCount.innerText = `${totalItems} item${totalItems > 1? 's':''}`
+    document.querySelectorAll(".cart-input").forEach((product) => product.addEventListener("click", updateCart));
+    
+}
 
 const addToCart = (event) => {
     const productKey = Number.parseInt(event.target.id);
-    const searchProduct = cartProducts.find(item => item.key === productKey);
+    const searchProduct = cartProducts.find(item => item.id === productKey);
 
     if (searchProduct != null){
         searchProduct.count += 1;
     }else{
         
-        const newCartProduct = products.find(item => item.key === productKey)
-        console.log(newCartProduct);
-        cartProducts.push({
-            count:1 , 
-            ...newCartProduct
-        })
+        const newCartProduct = products.find(item => item.id === productKey)
+        
+        if(newCartProduct){
+            cartProducts.push({
+                count:1 , 
+                ...newCartProduct
+            })
+        }else{
+            console.log("product not found")
+        }
     }
     
     console.log(cartProducts)
+
     renderCartProducts()
 }
 
 const updateCart = (event) => {
     const productKey = Number.parseInt(event.target.id);
     const productCount = Number.parseInt(event.target.value);
-    const searchProduct = cartProducts.find(item => item.key === productKey)
+    const searchProduct = cartProducts.find(item => item.id === productKey)
 
     if (searchProduct != null){
         searchProduct.count = productCount;
@@ -65,72 +105,4 @@ const updateCart = (event) => {
     renderCartProducts()
 }
 
-const renderCartProducts = () => {
-    cartProductList.innerHTML = null;
-    let total = 0;
-    let totalItems = 0;
-    cartProducts.forEach( item => {
-        const newProductItem = document.createElement('li');
-        newProductItem.setAttribute('class', 'list-group-item');
-
-        const newProduct = document.createElement('div');
-        newProduct.setAttribute('class', 'cart-product');
-        newProductItem.appendChild(newProduct)
-        
-        const newProductName = document.createElement('div');
-        newProductName.setAttribute('class', 'cart-product-name');
-        newProductName.innerText = ` ${item.productName.slice(0, 30)} ...`;
-
-        const newProductCost = document.createElement('div');
-        newProductCost.setAttribute('class', 'cart-product-cost');
-        newProductCost.innerText = `${item.productCost} Rs`;
-
-        const newProductButton = document.createElement('input');
-        newProductButton.setAttribute('type', 'number');
-        newProductButton.setAttribute('class', 'cart-product-input');
-        newProductButton.setAttribute('id', item.key);
-        newProductButton.setAttribute('value', item.count);
-        newProductButton.addEventListener('click', updateCart);
-    
-        newProduct.appendChild(newProductName);
-        newProduct.appendChild(newProductCost);
-        newProduct.appendChild(newProductButton);
-
-        cartProductList.appendChild(newProductItem);
-
-        total += item.count*item.productCost;
-        totalItems += item.count;
-    })
-
-    totalCost.innerText = `${total} Rs`;
-    itemsCount.innerText = `${totalItems} item${totalItems > 1? 's':''}`
-    
-}
-
-products.forEach(item => {
-    const newProductItem = document.createElement('li');
-    newProductItem.setAttribute('class', 'list-group-item');
-    
-    const newProduct = document.createElement('div');
-    newProduct.setAttribute('class', 'product');
-    newProductItem.appendChild(newProduct);
-
-    const newProductName = document.createElement('div');
-    newProductName.setAttribute('class', 'product-name');
-    newProductName.innerText = item.productName;
-
-    const newProductCost = document.createElement('div');
-    newProductCost.setAttribute('class', 'product-cost');
-    newProductCost.innerText = `${item.productCost} Rs`;
-
-    const newProductButton = document.createElement('button');
-    newProductButton.setAttribute('class', 'product-button');
-    newProductButton.setAttribute('id', item.key);
-    newProductButton.addEventListener('click', addToCart);
-
-    newProduct.appendChild(newProductName);
-    newProduct.appendChild(newProductCost);
-    newProduct.appendChild(newProductButton);
-
-    productList.appendChild(newProductItem);
-})
+loadProducts()
